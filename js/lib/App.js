804884efa2e8a2ce630338ccfde156bb308c6fa9
beefoo <brian@youaremyjoy.org>
2024-12-19 10:44:34 -0500

    implement panel

--- js/lib/App.js
+++ js/lib/App.js
@@ -1,5 +1,7 @@
+import Config from './Config.js';
 import Data from './Data.js';
 import Map from './Map.js';
+import Panel from './Panel.js';
 
 export default class App {
   constructor(options = {}) {
@@ -11,7 +13,11 @@ export default class App {
   async init() {
     this.map = new Map();
     this.data = new Data();
+    this.panel = new Panel();
     await this.data.load();
-    this.map.loadMarkers(this.data.items);
+    this.map.setData(this.data.items);
+    this.map.loadColorOptions(Config.colorBy);
+    this.map.loadMarkers();
+    this.map.loadListeners();
   }
 }
