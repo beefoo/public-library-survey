97a31f0ea1e40242216d37b78b4a5e860e218be2
beefoo <brian@youaremyjoy.org>
2024-12-18 19:19:26 -0500

    boilerplate ui

---
+++ js/lib/App.js
@@ -0,0 +1,13 @@
+export default class App {
+  constructor(options = {}) {
+    const defaults = {
+     
+    };
+    this.options = Object.assign(defaults, options);
+    this.init();
+  }
+
+  init() {
+    
+  }
+}
