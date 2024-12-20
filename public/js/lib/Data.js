59a38e62913b0738dcc14a5d75a4733b05d3d1c1
beefoo <brian@youaremyjoy.org>
2024-12-19 20:29:43 -0500

    display results

--- public/js/lib/Data.js
+++ public/js/lib/Data.js
@@ -1,8 +1,10 @@
 import itemsjs from '../vendor/itemsjs.js';
+import Helper from './Helper.js';
 
 export default class Data {
   constructor(options = {}) {
     const defaults = {
+      maxResults: 1000,
       src: 'data/2022-library-data.json',
     };
     this.options = Object.assign(defaults, options);
@@ -11,6 +13,7 @@ export default class Data {
 
   init() {
     this.data = [];
+    this.$results = document.getElementById('tab-results');
   }
 
   async load() {
@@ -19,11 +22,14 @@ export default class Data {
     const table = await response.json();
     const rows = this.constructor.parseTable(table);
     this.items = this.constructor.parseItems(rows);
+    this.results = this.items.slice();
+    this.renderResults();
   }
 
   static parseItems(rows) {
-    return rows.map((row) => {
+    return rows.map((row, i) => {
       const item = structuredClone(row);
+      item.originalIndex = i;
       return item;
     });
   }
@@ -37,5 +43,21 @@ export default class Data {
       });
       return item;
     });
+  }
+
+  renderResults() {
+    const { maxResults } = this.options;
+    let { results, $results } = this;
+    const count = results.length;
+    const population = Helper.sum(results, 'pop_lsa');
+    if (count > maxResults) results = results.slice(0, maxResults);
+    let html = '';
+    html += `<p>Found <strong>${count.toLocaleString()}</strong> libraries serving <strong>${population.toLocaleString()}</strong> people.</p>`;
+    html += '<ul class="result-list">';
+    results.forEach((item, i) => {
+      html += `<li><button data-index="${item.originalIndex}" class="result-button">${i + 1}. ${item.name} <small>(${item.city}, ${item.state})</small></button></li>`;
+    });
+    html += '</ul>';
+    $results.innerHTML = html;
   }
 }
